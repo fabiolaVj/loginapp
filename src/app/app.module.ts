773dec105c9569d197f4d6749c227b18d1fd23e4
fabiolaVj <faby_vallejo@hotmail.com>
2019-03-14 09:53:39 -0600

    Añadimos navbar y rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { HomePageComponent } from './home-page/home-page.component';
+import { HomePageComponent } from './componentes/home-page/home-page.component';
 import { NavbarComponent } from './componentes/navbar/navbar.component';
 import { RegisterPageComponent } from './componentes/register-page/register-page.component';
 import { LoginPageComponent } from './componentes/login-page/login-page.component';
